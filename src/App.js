00e032781ab22aef01ed7d10ca4d510fd613a09c
EthanKatungi <katungiethan@gmail.com>
2024-04-04 11:19:29 +0300

    modified:   src/App.js 	new file:   src/BlogList.js 	modified:   src/Home.js 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ import Home from './Home';
 
 function App() { 
   const myTitle = "Hello Reacters"
-  const germanPlayers = {
-    name: 'Joshua Kimmich',
-    club: 'Bayern',
-    position: 'F9',
-  }
   return (
     
     <div className="App">
       <Navbar />
       <div className="content">
         <Home />
-        <p>{germanPlayers.name} is a {germanPlayers.position} who plays for {germanPlayers.club}</p>
+        <p></p>
       </div>
     </div>
   );
